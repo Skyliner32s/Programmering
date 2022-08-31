35f1cd0ed394ca7efeba50961009a214283921b6
Lucas Milla <lucas.milla@elev.ga.ntig.se>
2022-08-31 14:22:27 +0200

    initial commit for lucktext

---
+++ Lucktext/Lucktext/Program.cs
@@ -0,0 +1,26 @@
+﻿//Console.WriteLine("Hej NTI");
+//Console.WriteLine("Hej TE21B");
+
+string name;
+string bodyPart;
+string fluid;
+string car;
+//name = "Lucas";
+//name = "Micke";
+
+Console.WriteLine("Vad heter du?");
+name = Console.ReadLine();
+
+Console.WriteLine($"Välkommen {name}!");
+Console.WriteLine("Skriv namnet på en kroppsdel");
+bodyPart = Console.ReadLine();
+
+Console.WriteLine("Skriv något flytande");
+fluid = Console.ReadLine();
+
+Console.WriteLine("Skriv ett bil märke");
+car = Console.ReadLine();
+
+Console.WriteLine("");
+
+Console.ReadLine();
